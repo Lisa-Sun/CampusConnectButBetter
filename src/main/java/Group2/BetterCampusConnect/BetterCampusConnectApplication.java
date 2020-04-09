6d69b93e632325824dc81c07d33cf1dff6c02356
Edward Lin <edward30525@gmail.com>
2020-04-08 19:58:53 -0500

    first commit

---
+++ src/main/java/Group2/BetterCampusConnect/BetterCampusConnectApplication.java
@@ -0,0 +1,13 @@
+package Group2.BetterCampusConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterCampusConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterCampusConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Group2/BetterCampusConnect/BetterCampusConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package Group2.BetterCampusConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterCampusConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
