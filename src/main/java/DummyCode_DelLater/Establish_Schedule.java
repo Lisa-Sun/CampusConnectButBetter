1f78a63a29400f847c0fba6d49c1a1275717b4e6
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-12 14:44:20 -0500

    Dummy/Skeleton Code for Student Schedule: Very rough draft

---
+++ src/main/java/DummyCode_DelLater/Establish_Schedule.java
@@ -0,0 +1,45 @@
+package DummyCode_DelLater;
+
+public class Establish_Schedule {
+	
+	
+	public void GetJSonPropertyList(String jsonTableName, String jsonPropertyName)
+	{
+		
+	}
+	
+	public void UpdatJsonProperty(String jsonTableName, String updatePropertyName, String updatedPropertyValue)
+	{
+		
+	}
+	
+	public void SetEmptySchedule()
+	{
+		//build an empty linked list 
+		//head -->Monday <-->Tuesday <-->Wednesday <--> ..Friday <-->Monday
+		//          |
+		//       Class node
+	}
+	
+	public void AddClassToSchedule()
+	{
+		
+	}
+	
+	public void ScheduleToJson()
+	{
+		
+	}
+	
+	public void JsonToSchedule()
+	{
+		
+	}
+	
+	public void GetDaysSchedule()
+	{
+		
+	}
+	
+	
+}
