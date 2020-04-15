8a83a9df75fad6064e1062f8a8d40ac0cc8604fc
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-15 13:59:34 -0500

    Added in Logger and CommandLineRunner within main.

--- src/main/java/Group2/BetterCampusConnect/BetterCampusConnectApplication.java
+++ src/main/java/Group2/BetterCampusConnect/BetterCampusConnectApplication.java
@@ -1,13 +1,41 @@
 package Group2.BetterCampusConnect;
 
+import Group2.BetterCampusConnect.model.Student;
+import Group2.BetterCampusConnect.model.StudentRepository;
+
+
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class BetterCampusConnectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BetterCampusConnectApplication.class, args);
 	}
+	
+	private static final Logger log = LoggerFactory.getLogger(BetterCampusConnectApplication.class);
+	
+	   @Bean
+	    public CommandLineRunner showAllCourses(StudentRepository repository) {
+	        return (args) -> {
+	            // fetch all customers
+	            log.info("Students found with findAll():");
+	            log.info("-------------------------------");
+	            repository.findAll().forEach((student) -> {
+	                log.info(student.toString());
+	            });
+	            log.info("-------------------------------");
+	        };
+	   }
+	   
 
 }
