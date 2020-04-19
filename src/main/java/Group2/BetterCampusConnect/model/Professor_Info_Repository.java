92ae3b848a7d65b5809900851a8296e004b996e9
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-18 22:07:21 -0500

    Add Prof_info_repo.java

---
+++ src/main/java/Group2/BetterCampusConnect/model/Professor_Info_Repository.java
@@ -0,0 +1,18 @@
+package Group2.BetterCampusConnect.model;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Professor_Info_Base_SQL_Repository
+ */
+
+
+public interface Professor_Info_Repository extends CrudRepository<Professor_Info, Long> {
+    List<Student> findByProfessorId(String professor_id); 
+    
+    @Override
+  
+    List<Professor_Info> findAll();
+}
+
