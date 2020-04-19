5bfa9e757934fac04f8c2fb049690c2ae0842fe5
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-18 20:18:31 -0500

    Create base SQL tables for professors

---
+++ src/main/resources/db/migration/V6__data.sql
@@ -0,0 +1,14 @@
+INSERT INTO professor_base_info
+	(professor_id,
+	first_name,
+	last_name,
+	department,
+	email)
+VALUES
+	('d1c2b3a4',
+	'Ada',
+	'Lovelace',
+	'Computer Science',
+	'ada.lovelace@mail.college.edu');
+
+
