a0f2c39f6a86ab8feaa8352cb8a445cab23e9fd9
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-18 21:39:54 -0500

    Add data point for course info

---
+++ src/main/resources/db/migration/V9__CourseInfoData.sql
@@ -0,0 +1,16 @@
+INSERT INTO course_base_info
+  (
+     course_id ,
+     course_name ,
+     department,
+     professor,
+     coordinator,
+     coord_email)
+  VALUES
+   ('CSC100',
+  	'Introduction to Computer Science',
+  	'Computer Science',
+  	'Ada Lovelace',
+  	'Alan Turing',
+  	'alan.turning@mail.college.edu'
+  	)
