99e6e9c06ef75ab02502a6a8bad828ae639a2fa9
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-18 22:06:35 -0500

    Add Professor_Info.java

---
+++ src/main/java/Group2/BetterCampusConnect/model/Professor_Info.java
@@ -0,0 +1,40 @@
+package Group2.BetterCampusConnect.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+
+
+@Data
+@Entity
+@Table(name = "professor_base_info")
+@ToString
+
+
+public class Professor_Info {
+	@Id
+	 @Column(name = "professor_id")
+	 private String professorId;
+	 
+	 @Column(name = "first_name")
+	 private String firstName;
+	 
+	 @Column(name = "last_name")
+	 private String lastName;
+	 
+	 @Column(name = "department")
+	 private String department;
+	 
+	 @Column(name = "email")
+	 private String email;
+
+}
