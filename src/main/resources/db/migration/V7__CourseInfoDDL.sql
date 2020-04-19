6bb98cb515cec612eecf35ed54e5f1bf7e2ea7c0
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-04-18 21:39:13 -0500

    There is a mistake in this one with the varchar of coures_name. Fixed in V8

---
+++ src/main/resources/db/migration/V7__CourseInfoDDL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE course_base_info
+  (
+     course_id   VARCHAR(15),
+     course_name VARCHAR(30),
+     department  VARCHAR(20),
+     professor   VARCHAR(50),
+     coordinator VARCHAR(50),
+     coord_email VARCHAR(40),
+     PRIMARY KEY (course_id)
+  );
