c950d075eac698712046b1cc17d679f1e9e76b3d
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-01 23:18:01 -0500

    Add Professor data

---
+++ src/main/resources/db/migration/V23__data.sql
@@ -0,0 +1,11 @@
+INSERT INTO professor_base_info
+VALUES
+	('d1c2b3a4',
+	'faculty',
+	'active',
+	'ada.lovelace@mail.college.edu',
+	'123-456-7890',
+	'Ada',
+	'Lovelace',
+	'Computer Science'
+	);
