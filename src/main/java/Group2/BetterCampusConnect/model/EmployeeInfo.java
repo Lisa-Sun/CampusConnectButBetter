69bc5a78fb4839c03c72c4a17a83e3eb0e563437
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-02 00:06:08 -0500

    Add inheritance annotation

--- src/main/java/Group2/BetterCampusConnect/model/EmployeeInfo.java
+++ src/main/java/Group2/BetterCampusConnect/model/EmployeeInfo.java
@@ -9,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -24,6 +25,7 @@ import lombok.ToString;
 
 @Data
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Table(name = "employee_info")
 @ToString
 public class EmployeeInfo implements Serializable{
