7493ea8c722395601ca751d89e3502b63e484562
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-01 23:16:29 -0500

    Professor database inheritance and creation needs to be behind the creation of the employee table

--- src/main/resources/db/migration/V5__ddl.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE professor_base_info
-  (
-     professor_id VARCHAR(10)NOT NULL,
-     first_name VARCHAR(25),
-     last_name  VARCHAR(25),
-     department VARCHAR(30),
-     email      VARCHAR(35),
-     PRIMARY KEY (professor_id)
-  );
