f623ec84b5ca59b072c84f0bd205b6099e4a12f8
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-11 22:11:45 -0500

    started on financial plan search

---
+++ src/main/java/Group2/BetterCampusConnect/controller/FinanceController.java
@@ -0,0 +1,42 @@
+package Group2.BetterCampusConnect.controller;
+
+import Group2.BetterCampusConnect.model.FinancialPlanOverview;
+import Group2.BetterCampusConnect.model.FinancialPlanOverviewRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+
+public class FinanceController {
+
+    @Autowired
+    FinancialPlanOverviewRepository FinancialPlanOverview;
+
+
+    @RequestMapping("/financialPlanningBrowswer")
+    public String FinancialPlan(Model model)
+    {
+        return "financialPlanningBrowswer";
+    }
+
+    @RequestMapping("/FinancialPlanning")
+    public String planSearch(@RequestParam(name = "query", required = false, defaultValue = "") String query, Model model)
+    {
+        if (query.equals("")) {
+            model.addAttribute("plans", FinancialPlanOverview.findAll());
+        }
+        return "finanicalPlanning";
+    }
+}
