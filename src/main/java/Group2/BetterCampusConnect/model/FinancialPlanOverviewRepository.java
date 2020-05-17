0d82258e1b5db341535f0ace4f20f11f0a69f0c4
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-17 17:36:34 -0500

    added to financial planning UI

--- src/main/java/Group2/BetterCampusConnect/model/FinancialPlanOverviewRepository.java
+++ src/main/java/Group2/BetterCampusConnect/model/FinancialPlanOverviewRepository.java
@@ -14,7 +14,7 @@ public interface FinancialPlanOverviewRepository extends CrudRepository<Financia
 	List<FinancialPlanOverview> findAll();
 	
 	@Query(
-		value = "SELECT * From financial_plan_overview",
+		value = "SELECT * FROM financial_plan_overview WHERE TABLE_NAME = 'financial_plan_overview'",
 		nativeQuery = true)
 	List<FinancialPlanOverview> search(String query);
 }
