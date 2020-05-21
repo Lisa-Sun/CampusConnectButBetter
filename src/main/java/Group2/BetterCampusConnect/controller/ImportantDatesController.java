66dc902dbd7e7015f64fb9928f51a56e9fc7dc4a
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-21 14:05:09 -0500

    Add in imports for ImpDates and Date util and time

--- src/main/java/Group2/BetterCampusConnect/controller/ImportantDatesController.java
+++ src/main/java/Group2/BetterCampusConnect/controller/ImportantDatesController.java
@@ -1,14 +1,19 @@
 package Group2.BetterCampusConnect.controller;
 
+import Group2.BetterCampusConnect.model.ImpDates;
+import Group2.BetterCampusConnect.model.ImpDatesRepository;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +22,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller 
 public class ImportantDatesController
 {
+	@Autowired
+	ImpDatesRepository impDatesRepo;
+
 	@RequestMapping("/ImportantDates")
     public String FinancialPlan(Model model)
     {
+    	model.addAttribute("ImpDates", impDatesRepo.findAll());
         return "/ImportantDates";
     }
+
+
 }
