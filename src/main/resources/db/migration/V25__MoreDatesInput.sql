98ff5dc16d07c4bfaf4f6b917149cb67e44ccfa3
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-22 12:49:22 -0500

    Add data for important dates

---
+++ src/main/resources/db/migration/V25__MoreDatesInput.sql
@@ -0,0 +1,27 @@
+INSERT INTO ImpDates
+(
+	date_id,
+	start_date,
+	end_date
+)
+VALUES
+(
+	'Finals',
+	'2020-05-22',
+	'2020-05-30'
+);
+
+
+INSERT INTO ImpDates
+(
+	date_id,
+	start_date,
+	end_date
+)
+VALUES
+(
+	'C_Holiday',
+	'2020-03-22',
+	'2020-03-30'
+);
+
