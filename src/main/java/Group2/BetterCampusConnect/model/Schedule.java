a9db5c82736b9939cf4ebc9f6ddf5a53cc5bf8d2
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-05-28 17:21:45 -0500

    Add nosql for student scheudle

---
+++ src/main/java/Group2/BetterCampusConnect/model/Schedule.java
@@ -0,0 +1,21 @@
+package Group2.BetterCampusConnect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.util.List;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule implements Serializable {
+	private String monday;
+	private String tuesday;
+    private String wednesday;
+    private String thursday;
+    private String friday;
+}
+
