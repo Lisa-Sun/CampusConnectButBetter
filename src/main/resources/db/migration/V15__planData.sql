8494c10b0d914d24b2361c228664ab13e8d3c1c4
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-31 19:52:08 -0500

    update to advisor page

--- src/main/resources/db/migration/V15__planData.sql
+++ src/main/resources/db/migration/V15__planData.sql
@@ -15,4 +15,22 @@ VALUES
 	'2',
 	'sciFund.com',
 	'Nikola Tesla'
+);
+INSERT INTO financial_plan_overview
+(
+	plan_id,
+	plan_type,
+	plan_amount,
+	num_recievers,
+	more_info_link,
+	plan_creator
+)
+VALUES
+(
+	'plan002',
+	'grant',
+	'1000',
+	'6',
+	'google.com',
+	'Carl Sagan'
 )
